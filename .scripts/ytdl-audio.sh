e021d990fd9d635bf673be5aeef9aac5459b5d98
fpkmatthi <skymatth@gmail.com>
2019-07-08 23:32:36 +0200

    Added yt-download script for mp3 audio

---
+++ .scripts/ytdl-audio.sh
@@ -0,0 +1,38 @@
+#!/bin/dash
+
+songname=$1
+link=$2
+
+### TODO: add song to old-data.txt and check if song is already present on download 
+usage()
+{
+    echo "Usage: ytdl-audio.sh <name> <link>"
+}
+
+main() {
+    youtube-dl -o "$HOME/Music/$songname.%(ext)s" --extract-audio --audio-format mp3 $link
+}
+
+if [ $# -eq 0 ] || [ $# -gt 2 ]; then
+    usage
+    exit 1
+fi
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -*)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+main
